1c1469e661ebf529dd9bd978d0ae4e20b000af99
yacinembodji <ymbodji20@gmail.com>
2025-10-12 21:44:50 +0000

    Mise à jour Accueil, Navbar et App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-import Acceuil from "./Components/Acceuil";
-
+import Accueil from "./Components/Acceuil";
+import Formulaire from "./Components/Formulaire";
+import Services from "./Components/Service";
 
 function App() {
+  const [page, setPage] = useState<"accueil" | "formulaire" | "services">("accueil");
+
   return (
-    <div>
-       {/* <h1>Navbar</h1> */}
-      {/* <h1>Acceuil</h1> */}
-     <Navbar />
-      <Acceuil />
-     
+    <Router>
+      <div className="min-h-screen">
+        <Navbar />
 
+        {/* Configuration du Router */}
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Accueil
+                onCommander={() => setPage("formulaire")}
+                onVoirServices={() => setPage("services")}
+              />
+            }
+          />
+          <Route
+            path="/commander"
+            element={<Formulaire onRetour={() => setPage("accueil")} />}
+          />
+          <Route
+            path="/services"
+            element={<Services onRetour={() => setPage("accueil")} />}
+          />
+        </Routes>
 
-    </div>
+        {/* Rendu conditionnel existant (tu peux le garder si tu veux naviguer par état) */}
+        {/* {page === "accueil" && (
+          <Accueil
+            onCommander={() => setPage("formulaire")}
+            onVoirServices={() => setPage("services")}
+          />
+        )} */}
+
+        {page === "formulaire" && (
+          <Formulaire onRetour={() => setPage("accueil")} />
+        )}
+
+        {page === "services" && (
+          <Services onRetour={() => setPage("accueil")} />
+        )}
+      </div>
+    </Router>
   );
 }
 
